bd2fc3959bc16ffe80b4a76d6c0c04aa6d0c7a25
田岡勇人 <taoka.trombone@gmail.com>
2023-06-21 19:12:29 +0900

    global.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
+import "~/styles/globals/index.scss";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
