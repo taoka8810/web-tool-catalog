d23d27a42798805b2af0574ac000a5a2af1071eb
田岡勇人 <taoka.trombone@gmail.com>
2023-06-25 11:02:39 +0900

    封印

--- src/server/api/routers/tool.ts
+++ src/server/api/routers/tool.ts
@@ -1,21 +1,45 @@
-import { prisma } from "~/server/db";
-import { createTRPCRouter, publicProcedure } from "../trpc";
-import { z } from "zod";
+// import { prisma } from "~/server/db";
+// import { createTRPCRouter, publicProcedure } from "../trpc";
+// import { z } from "zod";
+// import { Category, SubCategory } from "@prisma/client";
 
-export const toolRouter = createTRPCRouter({
-  index: publicProcedure.query(async () => {
-    return await prisma.tool.findMany({
-      include: {
-        category: true,
-        subCategories: true,
-      },
-    });
-  }),
-  // create: publicProcedure
-  //   .input(ToolCreateInputSchema)
-  //   .query(async ({ input }) => {
-  //     await prisma.tool.create({
-  //       name:
-  //     });
-  //   }),
-});
+// export const toolRouter = createTRPCRouter({
+//   index: publicProcedure.query(async () => {
+//     return await prisma.tool.findMany({
+//       include: {
+//         category: true,
+//         subCategories: true,
+//       },
+//     });
+//   }),
+//   create: publicProcedure
+//     .input(
+//       z.object({
+//         name: z.string(),
+//         description: z.string(),
+//         icon: z.string(),
+//         category: z.any().transform((d) => d as Category),
+//         subCategories: z.any().transform((d) => d as SubCategory[]),
+//       })
+//     )
+//     .query(async ({ input }) => {
+//       await prisma.tool.create({
+//         data: {
+//           name: input.name,
+//           description: input.description,
+//           icon: input.icon,
+//           category: {
+//             connect: {
+//               name: input.category.name,
+//               slug: input.category.slug,
+//             },
+//           },
+//           subCategories: {
+//             createMany: {
+//               data: input.subCategories,
+//             },
+//           },
+//         },
+//       });
+//     }),
+// });
