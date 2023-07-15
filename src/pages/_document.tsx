7c83fcaf9038e22751cd8dbfe04042c82d44bae2
田岡勇人 <taoka.trombone@gmail.com>
2023-07-16 01:15:02 +0900

    meta情報設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,25 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Noto+Sans+JP&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="description"
+          content="Web Tool PortalはWeb開発でよく利用される、CMS、データベース、ストレージ、ホスティングなどのサービスやツールを一覧で紹介するポータルサイトです。"
+        />
+        <meta name="keywords" content="web,一覧,ポータルサイト,ツール" />
+        <meta name="author" content="taoka8810" />
+        <meta property="og:url" content="https://web-tool-portal.com" />
+        <meta
+          property="og:title"
+          content="Web Tool Portal | Web開発に役立つツール・サービスの一覧サイト"
+        />
+        <meta
+          property="og:description"
+          content="Web Tool PortalはWeb開発でよく利用される、CMS、データベース、ストレージ、ホスティングなどのサービスやツールを一覧で紹介するポータルサイトです。"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Web Tool Portal" />
+        <meta property="og:image" content="/ogp.png" />
+        <title>Web Tool Portal</title>
       </Head>
       <body>
         <Main />
