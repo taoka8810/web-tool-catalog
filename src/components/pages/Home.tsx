ce32402c22bc6960c6ca6c0cfb65e15ac6bcbb38
田岡勇人 <taoka.trombone@gmail.com>
2023-07-16 02:52:07 +0900

    ニジイロ

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -18,6 +18,21 @@ export const HomePage: React.FC<HomeProps> = ({ allTools, allCategories }) => {
     setCategory(category);
   };
 
+  // Pennnの情報
+  const PennnInfo = {
+    id: 0,
+    name: "Pennn",
+    description: "本サイト開発者が運営する技術ブログ。コンテンツはほぼない。",
+    url: "https://pennn.me",
+    image: "http://pennn.me/wp-content/uploads/2023/01/screenshot.png",
+    category: {
+      id: 0,
+      name: "Special",
+      slug: "Special",
+      description: "",
+    },
+  };
+
   return (
     <div className={style.inner}>
       <div className={style.hamburger}>
@@ -48,6 +63,7 @@ export const HomePage: React.FC<HomeProps> = ({ allTools, allCategories }) => {
             .map((tool) => (
               <Card key={tool.id} {...tool} />
             ))}
+          <Card {...PennnInfo} />
         </div>
       </main>
     </div>
