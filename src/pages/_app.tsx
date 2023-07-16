39dfc9a1aea0ded0a3348b86d6fe2ff6e77ec93b
田岡勇人 <taoka.trombone@gmail.com>
2023-07-16 15:27:13 +0900

    gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,31 @@
 import { type AppType } from "next/app";
+import Script from "next/script";
 import "~/styles/globals/index.scss";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        async
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-D7KH36BXHW`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-D7KH36BXHW');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
