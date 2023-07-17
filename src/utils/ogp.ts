cab74cb2001cfe87b65826521d3085db16dc59fc
田岡勇人 <taoka.trombone@gmail.com>
2023-07-17 13:52:48 +0900

    使用パッケージ変更

--- src/utils/ogp.ts
+++ src/utils/ogp.ts
@@ -1,10 +1,10 @@
-import { getPageOGPMetadata } from "metagros/move/cometPunch";
+import { getOGP } from "ogp-getter";
 
 export const getOGPImage = async (url: string) => {
   const dummyImageURL =
     "https://generative-placeholders.glitch.me/image?width=1200&height=630&style=cellular-automata&cells=50";
   try {
-    const metadata = await getPageOGPMetadata(url);
+    const metadata = await getOGP(url);
     return metadata.image ? metadata.image : dummyImageURL;
   } catch {
     return dummyImageURL;
