9ec2cd1399c8ade2547f7adf2381425536b2b24d
田岡勇人 <taoka.trombone@gmail.com>
2023-07-17 20:27:29 +0900

    ike

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -1,16 +1,21 @@
 import metaFetcher from "meta-fetcher";
 import { translator } from "./deepl";
+import { getOGP } from "ogp-getter";
 
 export const translateDescription = async (url: string) => {
   // メタデータ取得
   const meta = await metaFetcher(url);
-  // const ogp = await getOGP(url);
+  const ogp = await getOGP(url);
 
   // 変数に格納
   const metaDescription = meta.metadata.description;
-  // const ogpDescription = ogp.description;
+  const ogpDescription = ogp.description;
 
-  const originalDescription = metaDescription ? metaDescription : undefined;
+  const originalDescription = metaDescription
+    ? metaDescription
+    : ogpDescription
+    ? ogpDescription
+    : undefined;
 
   // Deepl APIの文字数制限の節約のため、本番環境以外では翻訳処理をしない
   const isProduction = process.env.IS_PRODUCTION;
